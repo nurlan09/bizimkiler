ce2b692a35d18c72eff13d7a87ecba7724d0abc7
Mustafa Mustafayev <khalilzadehnurlan@gmail.com>
2021-04-02 18:31:41 +0400

    ilk kommit

---
+++ tekrarlamaq/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tekrarlamaq
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int eded1, eded2, cem;
+            eded1 = Convert.ToInt32(textBox1.Text);
+            eded2 = Convert.ToInt32(textBox2.Text);
+            cem = eded1 + eded2;
+            label1.Text = cem.ToString();
+
+        }
+    }
+}
